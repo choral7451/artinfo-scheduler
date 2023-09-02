e03f1ed2a0e8a194cde6370415d7238439fef569
Daniel Im <chorales@naver.com>
2023-09-02 23:49:15 +0900

    ADD. scheduler time updated

--- src/artinfo/application/scheduler/job-recruits.scheduler.ts
+++ src/artinfo/application/scheduler/job-recruits.scheduler.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { CrawlerService } from '@/artinfo/application/service/crawler/crawler.service';
+import { LogPayload } from '@/artinfo/domain/entities/server_log.entity';
+import { LOG_LEVEL } from '@/artinfo/interface/type/type';
+import { UtilLog } from '@/artinfo/utils/log';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class JobRecruitsScheduler {
-  constructor(private readonly crawlerService: CrawlerService) {}
+  constructor(
+    private readonly crawlerService: CrawlerService,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
 
-  @Cron('0 30 23 * * *')
+  @Cron('0 0 23 * * *')
   async crawlJobRecruits(): Promise<void> {
     const startTime = new Date();
-    console.log(`크롤링 실행 : ${startTime}`);
+    const logPayload: LogPayload = {
+      level: LOG_LEVEL.ERROR,
+      className: 'JobRecruitsScheduler',
+      functionName: 'crawlJobRecruits',
+      message: `크롤링 실행 : ${startTime}`,
+    };
+    this.eventEmitter.emit('log.created', logPayload);
 
     await this.crawlerService.crawlRecruitJobs();
 
     const endTime = new Date();
-    console.log(`크롤링 종료 : ${endTime} - 소요 시간 : ${this.elapsedTime(endTime.getTime() - startTime.getTime())}`);
+    logPayload.message = `크롤링 종료 : ${endTime} - 소요 시간 : ${this.elapsedTime(endTime.getTime() - startTime.getTime())}`;
+    this.eventEmitter.emit('log.created', logPayload);
   }
 
   private elapsedTime(ms: number): string {
