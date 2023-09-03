6717c1b634fcd84f79c14ff20fb610c4f7d1b80f
Daniel Im <chorales@naver.com>
2023-09-03 23:13:00 +0900

    Upt. scheduler test

--- src/artinfo/application/scheduler/job-recruits.scheduler.ts
+++ src/artinfo/application/scheduler/job-recruits.scheduler.ts
@@ -12,7 +12,7 @@ export class JobRecruitsScheduler {
     private readonly eventEmitter: EventEmitter2,
   ) {}
 
-  @Cron('0 8 23 * * *')
+  @Cron('0 14 23 * * *')
   async crawlJobRecruits(): Promise<void> {
     const startTime = new Date();
     const logPayload: LogPayload = {
